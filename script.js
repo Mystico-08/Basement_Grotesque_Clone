4c593862e287e53bc4375bc815387c675ed63d58
Mystico-08 <gantavyamishra@gmail.com>
2026-04-20 17:32:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+history.scrollRestoration = "manual";
+window.onload = function(){
+  window.scrollTo(0, 0);
+};
+
+
+
 function Clock() {
   const now = new Date();
   const time = now.toLocaleTimeString();
@@ -26,7 +33,26 @@ btn.addEventListener("click", () => {
   btn.style.display = "none";
 });
 
-const elements = document.querySelectorAll('.animate,.section-5,.Section-7');
+
+const buttons = document.querySelectorAll('.button');
+const toast = document.getElementById('toast');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const text = btn.textContent;
+
+        navigator.clipboard.writeText(text).then(() => {
+            toast.classList.add('show');
+
+            setTimeout(() => {
+                toast.classList.remove('show');
+            }, 1000);
+        });
+    });
+});
+
+
+const elements = document.querySelectorAll('.animate,.Section-7,.section-5');
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
@@ -38,7 +64,7 @@ const observer = new IntersectionObserver((entries) => {
 
 elements.forEach((el, i) => {
   observer.observe(el);
-  el.style.transitionDelay = `${i * 0.5}s`;
+  el.style.transitionDelay = `${Math.min(i * 0.2, 0.6)}s`;
 });
 
 
@@ -68,10 +94,18 @@ const sizeSlider = document.getElementById("size");
 const trackingSlider = document.getElementById("tracking");
 const leadingSlider = document.getElementById("leading");
 
+const sizeValue = document.querySelectorAll("#sizeValue");
+const trackingValue = document.querySelectorAll("#trackingValue");
+const leadingValue = document.querySelectorAll("#leadingValue");
+
 function Text() {
   text.style.fontSize = sizeSlider.value + "px";
   text.style.letterSpacing = trackingSlider.value + "px";
   text.style.lineHeight = leadingSlider.value;
+
+  sizeValue.forEach(el => el.textContent = sizeSlider.value + "px");
+  trackingValue.forEach(el => el.textContent = trackingSlider.value + "px");
+  leadingValue.forEach(el => el.textContent = leadingSlider.value);
 }
 
 sizeSlider?.addEventListener("input", Text);
@@ -105,3 +139,38 @@ if (box) {
     };
   }
 }
+
+window.addEventListener("scroll", function() {
+  var scrollY = window.scrollY;
+ 
+
+  document.querySelector(".hero-text.outline").style.transform = "translateY(" + (-scrollY * 0.08) + "px)";
+  document.querySelector(".hero-text.filled").style.transform = "translateY(" + (-scrollY * 0.18) + "px)";
+ 
+
+  document.querySelector(".BigA").style.transform = "translateY(" + (scrollY * 0.05) + "px)";
+  document.querySelector(".text-2").style.transform = "translateY(" + (scrollY * 0.02) + "px)";
+ 
+ 
+  var leftLines = document.querySelectorAll(".sec-6-left .sec-6-line");
+  leftLines[0].style.transform = "translateY(" + (scrollY * 0.03) + "px)";
+  leftLines[1].style.transform = "translateY(" + (scrollY * 0.04) + "px)";
+  leftLines[2].style.transform = "translateY(" + (scrollY * 0.05) + "px)";
+
+ var rightLines = document.querySelectorAll(".sec-6-right-box .sec-6-line");
+  rightLines[0].style.transform = "translateY(" + (scrollY * 0.03) + "px)";
+  rightLines[1].style.transform = "translateY(" + (scrollY * 0.04) + "px)";
+  rightLines[2].style.transform = "translateY(" + (scrollY * 0.05) + "px)";
+  
+  var bottomLines = document.querySelectorAll(".sec-6-bottom .sec-6-line");
+  bottomLines[0].style.transform = "translateY(" + (scrollY * 0.03) + "px)";
+  bottomLines[1].style.transform = "translateY(" + (scrollY * 0.04) + "px)";
+  bottomLines[2].style.transform = "translateY(" + (scrollY * 0.05) + "px)";
+  
+  document.querySelector(".sec8-poster").style.transform = "translateY(" + (-scrollY * 0.02) + "px)";
+  document.querySelector(".sec8-text").style.transform = "translateY(" + (-scrollY * 0.01) + "px)";
+
+  document.querySelector("#sec-9-text").style.transform = "translateY(" + (-scrollY * 0.02) + "px)";
+});
+
+ 
